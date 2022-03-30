1e8eb91768e711193480dad430e5ae438861de8e
cjeon <cjeon@student.42seoul.kr>
2022-03-30 16:27:09 +0900

    Add wordpress config

--- srcs/wordpress/files/run.sh
+++ srcs/wordpress/files/run.sh
@@ -2,8 +2,11 @@
 
 if [ ! -d '/srv/www/wordpress' ]; then
   cp -r /root/wordpress /srv/www/wordpress
+  cp /root/wp-config.php /srv/www/wordpress/wp-config.php
+  chown -R nobody:nobody /srv/www
 fi
 
 rm -rf /root/wordpress
+rm -f /root/wp-config.php
 
 php-fpm7 -F
