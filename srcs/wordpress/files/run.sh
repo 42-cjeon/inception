ad0e6ad73ee3e5fc0e41bc761140606fca892b0c
cjeon <cjeon@student.42seoul.kr>
2022-04-02 19:12:44 +0900

    Improved settings

--- srcs/wordpress/files/run.sh
+++ srcs/wordpress/files/run.sh
@@ -1,12 +1,23 @@
 #! /bin/sh
 
+set -eux
 if [ ! -d '/srv/www/wordpress' ]; then
-  cp -r /root/wordpress /srv/www/wordpress
-  cp /root/wp-config.php /srv/www/wordpress/wp-config.php
+  wp core download --path=/srv/www/wordpress
+  wp core config --path=/srv/www/wordpress --dbhost=mariadb \
+                 --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD"
+  wp core install --path=/srv/www/wordpress --url="$WP_URL" --title="$WP_TITLE" \
+                  --admin_name="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWORD" \
+                  --admin_email="$WP_ADMIN_EMAIL"
   chown -R nobody:nobody /srv/www
 fi
 
-rm -rf /root/wordpress
-rm -f /root/wp-config.php
+unset DB_NAME
+unset DB_USER
+unset DB_PASSWORD
+unset WP_URL
+unset WP_TITLE
+unset WP_ADMIN_NAME
+unset WP_ADMIN_PASSWORD
+unset WP_ADMIN_EMAIL
 
 php-fpm7 -F
