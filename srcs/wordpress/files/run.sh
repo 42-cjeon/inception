bfdd03eec7e66144fd711dc431f7972160ecfcfd
cjeon <cjeon@student.42seoul.kr>
2022-04-05 17:26:38 +0900

    Implement adminer

--- srcs/wordpress/files/run.sh
+++ srcs/wordpress/files/run.sh
@@ -9,8 +9,8 @@ if [ ! -d '/srv/www/wordpress' ]; then
                   --admin_name="$WP_ADMIN_NAME" --admin_password="$WP_ADMIN_PASSWORD" \
                   --admin_email="$WP_ADMIN_EMAIL"
   wp plugin install --path=/srv/www/wordpress redis-cache --activate
+  curl -L -o /srv/www/wordpress/adminer.php https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php
   sed -i "/Add any custom values between/ a define( 'WP_REDIS_HOST', 'redis' );" /srv/www/wordpress/wp-config.php
-  chown -R nobody:nobody /srv/www
 fi
 
 unset DB_NAME
